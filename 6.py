8d558064a162592ea67cc9b73cae1d38e826ec5d
Tamilvanan1236 <51222664+Tamilvanan1236@users.noreply.github.com>
2019-06-11 10:07:57 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,7 @@
+n=input()
+if(n=='a' or n=='e' or n=='i' or n=='o' or n=='u' or n=='A' or n=='E' or n=='I' or n=='O' or n=='U'):
+  print("Vowel")
+elif(n=='$' or n=='#' or n=='@' or n=='!' or n=='&'):
+  print("invalid")
+else:
+  print("Consonant")
